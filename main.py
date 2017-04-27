156746a12e0f1e7440ab829c841a5b3677f1589e
Christopher Dignam <chris@dignam.xyz>
2017-04-27 14:11:21 -0400

    WIP

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import argparse
 import os
+import time
+import datetime
+import pprint
 
 def validate_path(string):
     if os.path.isdir(string):
@@ -12,4 +15,21 @@ if __name__ == '__main__':
                     help='Path to screenshots folder.')
 
     args = parser.parse_args()
+    screenshots = os.listdir(args.path)
 
+    print(f'There are { len(screenshots) } items in this folder.')
+    years = {}
+    for screenshot in screenshots:
+        created = datetime.datetime.fromtimestamp(os.path.getctime(os.path.join(args.path, screenshot)))
+        modified = datetime.datetime.fromtimestamp(os.path.getmtime(os.path.join(args.path, screenshot)))
+        if created < modified:
+            try:
+                years[created.year] += 1
+            except KeyError:
+                years[created.year] = 1
+        else:
+            try:
+                years[modified.year] += 1
+            except KeyError:
+                years[modified.year] = 1
+    pprint.pprint(years)
